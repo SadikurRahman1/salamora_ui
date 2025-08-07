e180fcc9c08367f3def3c73408f0d3ca12d05d0a
mdarif3499 <arifhossen3499@gmail.com>
2025-08-07 10:10:00 +0600

    home screen loading

--- selemara/lib/features/car_owner_flow/home/view/home_screen_owner.dart
+++ selemara/lib/features/car_owner_flow/home/view/home_screen_owner.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:selemara/core/constants/app_icons.dart';
-import 'package:selemara/core/constants/app_images.dart';
 import 'package:selemara/core/widgets/app_text.dart';
-import 'package:selemara/core/widgets/custom_button.dart';
 import 'package:selemara/features/car_owner_flow/home/widget/information_card.dart';
 
 import '../../../../core/constants/app_colors.dart';
@@ -17,7 +15,7 @@ class HomeScreenOwner extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      appBar: CustomHomeAppbar(),
       body: Container(
         margin: EdgeInsets.symmetric(horizontal: 24),
 
@@ -25,44 +23,6 @@ class HomeScreenOwner extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-
-
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  children: [
-                    Image.asset(AppImages.userProfile),
-                    SizedBox(width: res.wp(5)),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        AppText(
-                          "John Doe",
-                          color: AppColors.textColor,
-                          fontSize: 16,
-                          fontWeight: FontWeight.w500,
-                        ),
-                        AppText(
-                          "Garage",
-                          color: AppColors.textColor.withValues(alpha: 0.8),
-                          fontSize: 12,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-                Container(
-                  padding: EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: AppColors.primaryColor.withValues(alpha: 0.2),
-                  ),
-                  child: Image.asset(AppIcons.notificationIcon),
-                ),
-              ],
-            ),
             SizedBox(height: res.hp(32)),
 
             Container(
@@ -169,22 +129,6 @@ class HomeScreenOwner extends StatelessWidget {
                     height: res.hp(67),
                     iconSize: res.hp(32),
                   ),
-                ),
-              ],
-            ),
-
-            SizedBox(height: res.hp(24)),
-
-            Row(
-              children: [
-                Expanded(
-                  child: CustomButton(text: "Add Vehicle", onTap: () {}),
-                ),
-
-                SizedBox(width: res.wp(20)),
-
-                Expanded(
-                  child: CustomButton(text: "Add Vehicle", onTap: () {},btnColor: AppColors.orange,),
                 ),
               ],
             ),
